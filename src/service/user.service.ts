0f474ccb6892afd9a53f65052f5e3cbb9faaa76b
derekhcchou <derekchc53@gmail.com>
2023-01-01 14:47:00 -0800

    #microsoft-user-collection add pagination retrieving

--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -3,10 +3,28 @@ import { getMSAuthData, MS_GQL_ENDPOINT } from './auth/microsoft.oauth.service';
 
 export const retrieveUsers = async () => {
   const { token } = await getMSAuthData();
-  const { data } = await axios.get(`${MS_GQL_ENDPOINT}users`, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
-  return data.value;
+  let user = [];
+  let nextPageLink = undefined;
+  const getDataPage = async () => {
+    try {
+      let url = `${MS_GQL_ENDPOINT}users?$top=100`;
+      if (nextPageLink) url = nextPageLink;
+      console.log('url', url);
+      return await axios.get(url, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+    } catch (e) {
+      console.log('Error', e);
+    }
+  };
+
+  while (true) {
+    const { data } = await getDataPage();
+    user.push(...data.value);
+    nextPageLink = data['@odata.nextLink'];
+    if (!data['@odata.nextLink']) break;
+  }
+  return user;
 };
